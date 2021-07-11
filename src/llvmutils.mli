d1c05ac08963698066bcb196c44c9239563ab666
hyunsukimsokcho <hskimsokcho@gmail.com>
2021-07-11 20:12:16 +0900

    adapt to thrillvm-analyzer

--- src/llvmutils.mli
+++ src/llvmutils.mli
@@ -16,7 +16,19 @@ val fold_left_all_instr :
 (** [fold_left_all_instr f a m] returns [f (... f (f (f a i1) i2) i3 ...) in], where [i1..in] are the instructions in [m]. *)
 
 val find_main : Llvm.llmodule -> Llvm.llvalue
-(** return a function with name [main] in given llmodule. If no such function exists, [failwith "main funtion not found"]. *)
+(** [find_main llm] returns a function with name [main] in a given llmodule [llm]. If no such function exists, [failwith "main funtion not found"]. *)
+
+val entry_point : Llvm.llvalue -> (Llvm.llbasicblock, Llvm.llvalue) Llvm.llpos
+(** [entry_point fn] returns the first position of the instruction list corresponding to the function [fn]. *)
+
+val get_function : Llvm.llvalue -> Llvm.llvalue
+(** [get_function instr] returns the function that contains [instr]. *)
+
+val get_next_nonphi : Llvm.llvalue -> (Llvm.llbasicblock, Llvm.llvalue) Llvm.llpos
+(** [get_next_nonphi instr] returns the position of the instruction that is not [Llvm.Opcode.PHI] after [instr]. *)
+
+val get_next_phi : Llvm.llvalue -> (Llvm.llbasicblock, Llvm.llvalue) Llvm.llpos
+(** [get_next_phi instr] returns the position of the instruction that is [Llvm.Opcode.PHI] after [instr]. *)
 
 val neg_pred : Llvm.Icmp.t -> Llvm.Icmp.t
 (** [neg_pred pr] is negated form of the predicate [pr]. *)
@@ -39,24 +51,36 @@ val is_binary_op : Llvm.Opcode.t -> bool
 (** check if a given LLVM instruction is a binary operator. *)
 
 val is_input : Llvm.llvalue -> bool
-(** when given llvalue is [Llvm.Opcode.Call], check if a callee is [input]. If not [Llvm.Opcode.Call], undefined behavior. *)
+(** [is_input instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [input]. *)
 
 val is_print : Llvm.llvalue -> bool
-(** when given llvalue is [Llvm.Opcode.Call], check if a callee is [print]. If not [Llvm.Opcode.Call], undefined behavior. *)
+(** [is_print instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [print]. *)
+
+val is_print_num : Llvm.llvalue -> bool
+(** [is_print_num instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [print_num]. *)
+
+val is_print_ptr : Llvm.llvalue -> bool
+(** [is_print_ptr instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [print_ptr]. *)
+
+val is_print_mem : Llvm.llvalue -> bool
+(** [is_print_mem instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [print_mem]. *)
+
+val is_malloc : Llvm.llvalue -> bool
+(** [is_malloc instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [malloc]. *)
 
 val is_source : Llvm.llvalue -> bool
-(** when given llvalue is [Llvm.Opcode.Call], check if a callee is [source]. If not [Llvm.Opcode.Call], undefined behavior. *)
+(** [is_source instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [source]. *)
 
 val is_sink : Llvm.llvalue -> bool
-(** when given llvalue is [Llvm.Opcode.Call], check if a callee is [sink]. If not [Llvm.Opcode.Call], undefined behavior. *)
+(** [is_sink instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [sink]. *)
 
 val is_sanitizer : Llvm.llvalue -> bool
-(** when given llvalue is [Llvm.Opcode.Call], check if a callee is [sanitizer]. If not [Llvm.Opcode.Call], undefined behavior. *)
-
-val is_llvm_function : Llvm.llvalue -> bool
+(** [is_sanitizer instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is [sanitizer]. *)
 
 val is_llvm_intrinsic : Llvm.llvalue -> bool
-(** check if a given LLVM instruction is a LLVM intrinsic function. *)
+(** [is_llvm_intrinsic instr] checks if [instr] is [Llvm.Opcode.Call] and the callee is a LLVM intrinsic function. *)
+
+val is_llvm_function : Llvm.llvalue -> bool
 
 (** {2 Pretty printers} *)
 
@@ -68,6 +92,9 @@ val string_of_lhs : Llvm.llvalue -> string
 
 val string_of_exp : Llvm.llvalue -> string
 (** [string_of_exp i] returns the string representation of a LLVM expression [i]. *)
+
+val string_of_function : Llvm.llvalue -> string
+(** [string_of_exp fn] returns the string representation of a LLVM expression of a function [fn]. *)
 
 val string_of_location : Llvm.llcontext -> Llvm.llvalue -> string
 (** [string_of_location ctx i] returns the string representation of the location of a LLVM instruction [i]. *)
